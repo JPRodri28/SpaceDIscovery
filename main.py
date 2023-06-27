21c7cebbdd6da8aede699c1611d35e8740bd1da8
João Pedro Rodrigues <joperod2804@gmail.com>
2023-06-27 14:45:17 -0300

    Carregando e salvando posições de estrelas

--- main.py
+++ main.py
@@ -21,3 +21,28 @@ linha_ativa = False
 posicao_inicial = None
 posicao_final = None
 arquivo_estrelas = "estrelas.txt"
+def carregar_estrelas():
+    bolinhas.clear()
+    linhas.clear()
+    with open(arquivo_estrelas, "r") as arquivo:
+        for linha in arquivo:
+            posicao_x, posicao_y, legenda = linha.strip().split(",")
+            posicao = (int(posicao_x), int(posicao_y))
+            bolinha = {
+                "posicao": posicao,
+                "legenda": legenda
+            }
+            bolinhas.append(bolinha)
+            if len(bolinhas) >= 2:
+                posicao_anterior = bolinhas[-2]["posicao"]
+                posicao_atual = bolinha["posicao"]
+                linha = (posicao_anterior, posicao_atual)
+                linhas.append(linha)
+
+def salvar_estrelas():
+    with open(arquivo_estrelas, "w") as arquivo:
+        for bolinha in bolinhas:
+            posicao_x = str(bolinha["posicao"][0])
+            posicao_y = str(bolinha["posicao"][1])
+            legenda = bolinha["legenda"]
+            arquivo.write(posicao_x + "," + posicao_y + "," + legenda + "\n")
